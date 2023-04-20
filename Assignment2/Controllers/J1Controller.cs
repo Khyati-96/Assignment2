ea28a13f33dc212c91f4694cc32000e6cbe9890d
Khyati Sethi <sethikhyati96@gmail.com>
2023-04-19 20:35:36 -0400

    Add project files.

---
+++ Assignment2/Controllers/J1Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+    public class J1Controller : ApiController
+    {
+        [HttpGet]
+        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+        public string CalculateTotalCalories(int burger, int drink, int side, int dessert)
+        {
+            int[] burgerCalories = { 461, 431, 420, 0 };
+            int[] drinkCalories = { 130, 160, 118, 0 };
+            int[] sideCalories = { 100, 57, 70, 0 };
+            int[] dessertCalories = { 167, 266, 75, 0 };
+
+            int totalCalories = burgerCalories[burger - 1] + drinkCalories[drink - 1] + sideCalories[side - 1] + dessertCalories[dessert - 1];
+
+            return $"Your total calorie count is {totalCalories}";
+        }
+    }
+}
